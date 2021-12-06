217e6916ba25dc9aaccce0ec2c23eb5e7bf07d70
take-2405 <take12.wa@icloud.com>
2021-12-07 06:16:28 +0900

    add 最低限動作するメニューバーアプリケーションを作成

---
+++ storyBoardGetWindow/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  storyBoardGetWindow
+//
+//  Created by Taketo Wakamatsu on 2021/12/07.
+//
+
+import Cocoa
+
+import SwiftUI
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var popover = NSPopover.init()
+    var statusBar: StatusBarController?
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the contents
+        let contentView = ContentView()
+
+        // Set the SwiftUI's ContentView to the Popover's ContentViewController
+        popover.contentSize = NSSize(width: 360, height: 360)
+        popover.contentViewController = NSHostingController(rootView: contentView)
+
+        // Create the Status Bar Item with the above Popover
+        statusBar = StatusBarController.init(popover)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
---
+++ storyBoardGetWindow/StatusBarConrtroller.swift
@@ -0,0 +1,49 @@
+//
+//  StatusBarConrtroller.swift
+//  storyBoardGetWindow
+//
+//  Created by Taketo Wakamatsu on 2021/12/07.
+//
+import Foundation
+import AppKit
+
+class StatusBarController {
+    private var statusBar: NSStatusBar
+    private var statusItem: NSStatusItem
+    private var popover: NSPopover
+
+    init(_ popover: NSPopover) {
+        self.popover = popover
+        statusBar = NSStatusBar.init()
+        // Creating a status bar item having a fixed length
+        statusItem = statusBar.statusItem(withLength: 28.0)
+
+        if let statusBarButton = statusItem.button {
+            // 追加したアイコン名を設定
+            statusBarButton.image = #imageLiteral(resourceName: "icon")
+            statusBarButton.image?.size = NSSize(width: 18.0, height: 18.0)
+            statusBarButton.image?.isTemplate = true
+            statusBarButton.action = #selector(togglePopover(sender:))
+            statusBarButton.target = self
+        }
+    }
+
+    @objc func togglePopover(sender: AnyObject) {
+        if(popover.isShown) {
+            hidePopover(sender)
+        }
+        else {
+            showPopover(sender)
+        }
+    }
+
+    func showPopover(_ sender: AnyObject) {
+        if let statusBarButton = statusItem.button {
+            popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: NSRectEdge.maxY)
+        }
+    }
+
+    func hidePopover(_ sender: AnyObject) {
+        popover.performClose(sender)
+    }
+}
